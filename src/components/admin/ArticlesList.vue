b968df7d6e69b06d02e7a1963a5e324329d12c24
DhrutiUpadhyay <37285944+DhrutiUpadhyay@users.noreply.github.com>
2018-08-10 11:11:11 +0530

    Folder Structure

---
+++ src/components/admin/ArticlesList.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Articles List (Admin)</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/users/CreatedArticles.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Created Articles (User)</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
